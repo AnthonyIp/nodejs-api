6cc2ceae42d31abc5b5386382243eb08beac4883
anthony <anthonyip@hotmail.fr>
2019-11-25 00:40:01 +0100

    * Create "forgot password"

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,12 @@ const {
     register,
     login,
     getMe,
+    forgotPassword,
 } = require('../controllers/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
