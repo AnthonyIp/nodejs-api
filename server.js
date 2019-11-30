e9760013395a6479fe503b1ae990f1c4ba8cb18e
anthony <anthonyip@hotmail.fr>
2019-11-30 03:19:00 +0100

    * Add XSS attacks security

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const cookieParser  = require('cookie-parser');
 const errorHandler  = require('./middleware/error');
 const connectDB     = require('./config/db');
 const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
+const helmet        = require('helmet');
+const xss           = require('xss-clean');
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
@@ -45,6 +46,9 @@ app.use(mongoSanitize());
 
 // Set security Helmet
 app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
