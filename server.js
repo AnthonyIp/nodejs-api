f38af862bae8f616e8bd13453cc017324ded3ec3
anthony <anthonyip@hotmail.fr>
2019-11-30 03:10:07 +0100

    * Sanitize data

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const fileupload    = require('express-fileupload');
 const cookieParser  = require('cookie-parser');
 const errorHandler  = require('./middleware/error');
 const connectDB     = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
@@ -37,6 +39,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // file uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
